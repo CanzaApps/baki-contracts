9c5e27455cc37565259282eec9514d3b03078108
Deon Achuo <sird655@gmail.com>
2022-05-26 10:15:39 +0100

    created zUSD and vault

--- test/vault.test.js
+++ test/vault.test.js
@@ -3,9 +3,7 @@ const Vault = artifacts.require("Vault");
 contract("Vault", accounts => {
   it("should deposit cUSD", async () => {
     const vault = await Vault.deployed();
-    await vault.deposit(web3.utils.toWei("1", "ether"), {
-      from: "0x5488396763CcD71B64c68eD45fF831D39257a254",
-    });
+    await vault.deposit(web3.utils.toWei("1", "ether"), { from: accounts[0] });
     const getVault = await vault.getVault(accounts[0]);
     assert.equal(getVault.collateralAmount, web3.utils.toWei("1", "ether"));
   });
