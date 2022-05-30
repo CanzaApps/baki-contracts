536823e81828c74d070c92a0d076bbf3c6389dfd
Deon Achuo <sird655@gmail.com>
2022-05-30 15:26:12 +0100

    created oracle

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -10,9 +10,11 @@ contract PriceOracle is ChainlinkClient, ConfirmedOwner {
 
     bytes32 private jobId;
     uint256 private fee;
-    uint256 public data;
-
-    string constant baseURL = "http://localhost:3000/api/v1/";
+    uint256 public CELOUSD;
+    uint256 public NGNUSD;
+    uint256 public CFAUSD;
+    uint256 public ZARUSD;
+    string constant baseURL = "https://baki-price-oracle.herokuapp.com/api/v1";
 
     event NewQuery(string description);
     event ResultObtained(bytes32 indexed requestId, uint256 result);
@@ -30,21 +32,19 @@ contract PriceOracle is ChainlinkClient, ConfirmedOwner {
         setChainlinkToken(0xa36085F69e2889c224210F603D836748e7dC0088);
         setChainlinkOracle(0x74EcC8Bdeb76F2C6760eD2dc8A46ca5e581fA656);
         jobId = "ca98366cc7314957b8c012c72f05aeeb";
-        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+        fee = 0.01 * 10**18; // 0,1 * 10**18 (Varies by network and job)
     }
 
-    /**
-     * Create a Chainlink request to retrieve API response, find the target
-     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
-     */
-    function submitRequest(string memory url)
+    // ############################################################################# CFAUSD #############################################################################
+
+    function submitCELOUSD(string memory url)
         public
         returns (bytes32 requestId)
     {
         Chainlink.Request memory req = buildChainlinkRequest(
             jobId,
             address(this),
-            this.fulfill.selector
+            this.fulfillCELOUSD.selector
         );
 
         // Set the URL to perform the GET request on
@@ -62,18 +62,157 @@ contract PriceOracle is ChainlinkClient, ConfirmedOwner {
     /**
      * Receive the response in the form of uint256
      */
-    function fulfill(bytes32 _requestId, uint256 result)
+    function fulfillCELOUSD(bytes32 _requestId, uint256 result)
         public
         recordChainlinkFulfillment(_requestId)
     {
         emit ResultObtained(_requestId, result);
-        data = result;
+        CELOUSD = result;
+    }
+
+    // ############################################################################# NGNUSD #############################################################################
+
+    function submitNGNUSD(string memory url)
+        public
+        returns (bytes32 requestId)
+    {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillNGNUSD.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add("get", url);
+        req.add("path", "data"); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10**18;
+        req.addInt("times", timesAmount);
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+
+    /**
+     * Receive the response in the form of uint256
+     */
+    function fulfillNGNUSD(bytes32 _requestId, uint256 result)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        emit ResultObtained(_requestId, result);
+        NGNUSD = result;
+    }
+
+    // ############################################################################# CFAUSD #############################################################################
+    function submitCFAUSD(string memory url)
+        public
+        returns (bytes32 requestId)
+    {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillCFAUSD.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add("get", url);
+        req.add("path", "data"); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10**18;
+        req.addInt("times", timesAmount);
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+
+    /**
+     * Receive the response in the form of uint256
+     */
+    function fulfillCFAUSD(bytes32 _requestId, uint256 result)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        emit ResultObtained(_requestId, result);
+        CFAUSD = result;
+    }
+
+    // ############################################################################# ZARUSD #############################################################################
+    function submitZARUSD(string memory url)
+        public
+        returns (bytes32 requestId)
+    {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillCFAUSD.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add("get", url);
+        req.add("path", "data"); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10**18;
+        req.addInt("times", timesAmount);
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+
+    /**
+     * Receive the response in the form of uint256
+     */
+    function fulfillZARUSD(bytes32 _requestId, uint256 result)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        emit ResultObtained(_requestId, result);
+        ZARUSD = result;
     }
 
     // create a request
     function createRequest(string memory pricePair) public {
         string memory url = string(abi.encodePacked(baseURL, "/", pricePair));
         emit NewQuery("Requesting price data");
-        submitRequest(url);
+        if (
+            keccak256(abi.encodePacked(pricePair)) ==
+            keccak256(abi.encodePacked("CELOUSD"))
+        ) {
+            submitCELOUSD(url);
+        } else if (
+            keccak256(abi.encodePacked(pricePair)) ==
+            keccak256(abi.encodePacked("NGNUSD"))
+        ) {
+            submitNGNUSD(url);
+        } else if (
+            keccak256(abi.encodePacked(pricePair)) ==
+            keccak256(abi.encodePacked("CFAUSD"))
+        ) {
+            submitCFAUSD(url);
+        } else if (
+            keccak256(abi.encodePacked(pricePair)) ==
+            keccak256(abi.encodePacked("ZARUSD"))
+        ) {
+            submitZARUSD(url);
+        }
+    }
+
+    function getCELOUSD() public view returns (uint256) {
+        return CELOUSD;
+    }
+
+    function getNGNUSD() public view returns (uint256) {
+        return NGNUSD;
+    }
+
+    function getCFAUSD() public view returns (uint256) {
+        return CFAUSD;
+    }
+
+    function getZARUSD() public view returns (uint256) {
+        return ZARUSD;
     }
 }
--- oracleServer/controller/priceController.js
+++
@@ -1,8 +0,0 @@
-class PriceController {
-  getCELOUSD(req, res) {}
-  getCFAUSD(req, res) {}
-  getNGNUSD(req, res) {}
-  getZARUSD(req, res) {}
-}
-
-module.exports = PriceController;
