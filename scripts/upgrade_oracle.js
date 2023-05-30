0bcdc1a7cd72c9b87c1d6cad6398db944f950bfe
Deon Achuo <sird655@gmail.com>
2023-05-30 17:55:07 +0100

    Updated Oracle contract

--- scripts/upgrade_oracle.js
+++ scripts/upgrade_oracle.js
@@ -1,6 +1,6 @@
 const { ethers, upgrades } = require("hardhat");
 
-// Current Address of the BakiOracle
+// Current Address of the Baki Oracle
 const currentOracleAddress = "0x3ab5E7a3466d0e5B556e0005D705cbC3ADc34767";
 
 async function main() {
