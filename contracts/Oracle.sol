3cc6645ef5eaeb2853a3efd743e6848521b6f0cc
David Asamonye <davidasamonye@gmail.com>
2023-08-20 19:04:28 +0100

    fixed vulnerabilities with adding and removing zTokens

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -8,6 +8,8 @@ contract BakiOracle is Ownable, BakiOracleInterface {
 
     mapping(string => address) private zTokenAddress;
     mapping(address => uint256) private zTokenUSDValue;
+    mapping(string => bool) private zTokenExists;
+    
     string[] public zTokenList;
     uint256 public collateralUSD;
 
@@ -18,9 +20,10 @@ contract BakiOracle is Ownable, BakiOracleInterface {
 
     function addZToken(string calldata _name, address _address) external onlyOwner {
         require(_address != address(0), "Address is invalid");
+        require(!checkIfTokenExists(_name), "zToken already exists");
 
         zTokenAddress[_name] = _address;
-
+        zTokenExists[_name] = true;
         zTokenList.push(_name);
 
         emit AddZToken(_name, _address);
@@ -36,10 +39,12 @@ contract BakiOracle is Ownable, BakiOracleInterface {
         return zTokenList;
     }
 
-     function removeZToken(string calldata _name) external onlyOwner {
+    function removeZToken(string calldata _name) external onlyOwner {
         require(zTokenAddress[_name] != address(0), "zToken does not exist");
+        require(zTokenExists[_name], "zToken does not exists");
 
         delete zTokenAddress[_name];
+        zTokenExists[_name] = false;
 
         uint256 index;
         bytes32 nameHash = keccak256(bytes(_name));
@@ -56,8 +61,22 @@ contract BakiOracle is Ownable, BakiOracleInterface {
             zTokenList.pop();
         }
 
-    emit RemoveZToken(_name);
-}
+        emit RemoveZToken(_name);
+    }
+
+    function checkIfTokenExists(string calldata _name) public view returns(bool){
+        require(!zTokenExists[_name], "zToken already exists");
+        
+        bytes32 nameHash = keccak256(bytes(_name));
+
+        for (uint256 i = 0; i < zTokenList.length; i++) {
+            if (keccak256(bytes(zTokenList[i])) == nameHash) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     function setZTokenUSDValue(
         string calldata _name,
