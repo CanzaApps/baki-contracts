e85686793c0354623ba1d0837a13f734dcb38e67
David Asamonye <davidasamonye@gmail.com>
2023-08-20 18:07:47 +0100

    fixed vulnerability with remove token function

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -36,13 +36,28 @@ contract BakiOracle is Ownable, BakiOracleInterface {
         return zTokenList;
     }
 
-    function removeZToken(string calldata _name) external onlyOwner {
+     function removeZToken(string calldata _name) external onlyOwner {
         require(zTokenAddress[_name] != address(0), "zToken does not exist");
 
         delete zTokenAddress[_name];
 
-        emit RemoveZToken(_name);
-    }
+        uint256 index;
+        bytes32 nameHash = keccak256(bytes(_name));
+
+        for (uint256 i = 0; i < zTokenList.length; i++) {
+            if (keccak256(bytes(zTokenList[i])) == nameHash) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index < zTokenList.length) {
+            zTokenList[index] = zTokenList[zTokenList.length - 1];
+            zTokenList.pop();
+        }
+
+    emit RemoveZToken(_name);
+}
 
     function setZTokenUSDValue(
         string calldata _name,
