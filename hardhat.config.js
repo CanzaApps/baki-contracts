f88cdfef353b908a76b4eb36e64c54f21e0257c6
David Asamonye <davidasamonye@gmail.com>
2023-09-04 14:56:07 +0100

    dummy private key for slither action

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,20 +20,20 @@ module.exports = {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/MIaPbNNNaHPp9qf5cDCOEJPVxJ_GwLVY`,
       accounts: [
-        process.env.TESTNET_PRIVATE_KEY || "123456",
+        process.env.TESTNET_PRIVATE_KEY || "ce823841483ccf5d4d6f35fc552fc6fb86a53473ff9fcae9f32b7ebb7a3af960",
       ],
     },
     fuji: {
       url: `https://omniscient-restless-thunder.avalanche-testnet.discover.quiknode.pro/933a2d24cf1a762fd73a569be25bdec21cb60a9f/ext/bc/C/rpc`,
       accounts: [
-        process.env.TESTNET_PRIVATE_KEY || "123456",
+        process.env.TESTNET_PRIVATE_KEY || "ce823841483ccf5d4d6f35fc552fc6fb86a53473ff9fcae9f32b7ebb7a3af960",
       ],
       chainId: 43113,
     },
     mainnet: {
       url: `https://red-neat-putty.avalanche-mainnet.quiknode.pro/d54c9d0935c483e2f8e70b7a756d882e45cd9e3f/ext/bc/C/rpc/`,
       accounts: [
-        process.env.MAINNET_PRIVATE_KEY || "123456",
+        process.env.MAINNET_PRIVATE_KEY || "ce823841483ccf5d4d6f35fc552fc6fb86a53473ff9fcae9f32b7ebb7a3af960",
       ],
       chainId: 43114
     }
